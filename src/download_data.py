d63870bca1d374fc5090a6d7aefc2c7fbd1189b8
Aagam Shah <aagam@redhat.com>
2018-01-16 15:41:18 +0530

    Sync remote and local

---
+++ src/download_data.py
@@ -0,0 +1,13 @@
+import boto3
+import botocore
+import os
+
+session = boto3.session.Session(aws_access_key_id=os.environ.get('AWS_S3_ACCESS_KEY', ''),
+                                             aws_secret_access_key=os.environ.get('AWS_S3_SECRET_KEY', ''))
+s3 = session.resource('s3', config=botocore.client.Config(
+            signature_version='s3v4'))
+
+bucket = s3.Bucket(os.environ.get("AWS_S3_BUCKET_NAME", ""))
+
+bucket.download_file(os.environ.get("AWS_S3_FILE_PATH", ""), os.environ.get("LOCAL_FILE_PATH", ""))
+
